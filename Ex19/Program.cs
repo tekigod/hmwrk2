fac3362c439018c452667f26eadaa59a89e28e6c
bladeshape <tekigod@mail.ru>
2022-08-19 04:35:27 +0300

    ex19

--- Ex19/Program.cs
+++ Ex19/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. принимает на вход пятизначное число и 
+// 2. проверяет, является ли оно палиндромом.
+// Например:
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+  if (number[0]==number[4] || number[1]==number[3]){
+    Console.WriteLine($"Ваше число: {number} - является палиндромом.");
+  }
+  else Console.WriteLine($"Ваше число: {number} - не является палиндромом.");
+}
+
+if (number!.Length == 5){
+  CheckingNumber(number);
+}
+else Console.WriteLine($"Введите пятизначное число!");
