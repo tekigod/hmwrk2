be6a573e4f994841bc92113cc908961688e26a3a
bladeshape <tekigod@mail.ru>
2022-08-19 04:55:30 +0300

    cellrdy

---
+++ Ex23/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход число (N) и 
+// 2. выдаёт таблицу кубов чисел от 1 до N.
+// Например: 
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Write("Введите число: ");
+int cell = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cell){
+  int counter = 0;
+  int length = cell.Length;
+  while (counter <  length){
+    cell[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] CL222){
+  int count = CL222.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(CL222[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cell+1];
+Cube(arry);
+PrintArry(arry);
